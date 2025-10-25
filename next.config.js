01384b756bb8aeecffe48f28d5686f4a5957ab03
Coimbraitalo01 <raulseixasaeon@gmail.com>
2025-10-25 14:42:26 -0300

    Resolve merge conflicts

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/Portal-Novatte' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portal-Novatte/' : '',
+  images: {
+    unoptimized: true,
+  },
+  // Adicione outras configurações do Next.js aqui, se necessário
+}
+
+module.exports = nextConfig
